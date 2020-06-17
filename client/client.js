a30d394b0eee87637999c7f2f89384f0b22e69de
joshfagen <j.fagen@muxlab.com>
2020-06-17 01:21:02 -0400

    initates server

--- client/client.js
+++ client/client.js
@@ -1,6 +1,21 @@
 const form = document.querySelector('form');
 
+const loading = document.querySelector('.loading'); 
+loading.style.display = 'none';
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('form submitted!!');
+    
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const message = formData.get('message');
+
+    const woof = {
+        name,
+        message
+    };
+
+    form.style.display = 'none';
+    loading.style.display = '';
+    console.log(woof);
 });
