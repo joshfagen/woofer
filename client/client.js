fad352536e82107f49e293654927ed963a4ec0ee
joshfagen <j.fagen@muxlab.com>
2020-06-22 23:23:00 -0400

    woofs appear on page

--- client/client.js
+++ client/client.js
@@ -1,10 +1,13 @@
 const form = document.querySelector('form');
 
 const loading = document.querySelector('.loading'); 
-loading.style.display = 'none';
+loading.style.display = '';
+
+const woofsElement = document.querySelector('.woofs');
 
 const API_URL = 'http://localhost:5000/woofs';
 
+listAllWoofs(); //#endregion
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     
@@ -34,3 +37,26 @@ form.addEventListener('submit', (e) => {
     });
 });
 
+function listAllWoofs() {
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(woofs => {
+            console.log(woofs);
+            woofs.forEach(woof => {
+                const div = document.createElement('div');
+                
+                const header = document.createElement('h3');
+                header.textContent = woof.name;
+
+                const contents = document.createElement('p');
+                contents.innerHTML = '<i>' + woof.message + '</i>';
+
+                div.appendChild(header);
+                div.appendChild(contents);
+
+                woofsElement.appendChild(div);
+
+            });
+        });
+
+}
